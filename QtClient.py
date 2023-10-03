96ee96e4c3643e4df005b423b3c8c06d7e8edcd5
S <146825225+GeckoDoesCoding@users.noreply.github.com>
2023-10-03 15:24:35 +0530

    Add files via upload

---
+++ QtClient.py
@@ -0,0 +1,48 @@
+import socket
+import time
+import sys
+from PyQt6.QtWidgets import *
+from PyQt6 import QtCore, QtGui
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+
+class Window(QMainWindow):
+
+	def __init__(self):
+		super().__init__()
+
+		# setting title
+		self.setWindowTitle("RP Adjust")
+		self.setWindowIcon(QtGui.QIcon('C:/users/shoukulk/Desktop/work/Logos/MagnaLogo.png'))
+		# setting geometry
+		self.setGeometry(300, 300, 550, 250)
+		self.UiComponents()
+		self.show()
+  
+	def UiComponents(self):
+
+		dial = QDial(self)
+		dial.setGeometry(50, 50, 150, 150)
+		dial.setRange(0, 20)
+		dial.setNotchesVisible(True)
+		
+		label = QLabel("Set Roll", self)
+		label.setGeometry(100, 175, 75, 75)
+		label.setWordWrap(True)
+		dial.valueChanged.connect(lambda: label.setText("Roll is: \n" +str(dial.value())+" Deg"))
+		#print(str(dial.value()))
+  
+		dial2 = QDial(self)
+		dial2.setGeometry(350, 50, 150, 150)
+		dial2.setRange(0, 20)
+		dial2.setNotchesVisible(True)
+        
+		label2 = QLabel("Set Pitch", self)
+		label2.setGeometry(400, 175, 75, 75)
+		label2.setWordWrap(True)
+		dial2.valueChanged.connect(lambda: label2.setText("Pitch is: \n" +str(dial2.value())+" Deg"))
+
+       	
+App = QApplication(sys.argv)
+window = Window()
+sys.exit(App.exec())
